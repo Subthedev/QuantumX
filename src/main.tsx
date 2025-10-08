f1f69caa1b836ffe1f9b362cfcaa713255c90c45
Subthedev <contactsubhrajeet@gmail.com>
2025-10-08 22:24:05 +0530

    Fix: Remove StrictMode and fix hooks rules violation

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
@@ -6,8 +5,4 @@ import './index.css'
 const rootElement = document.getElementById("root");
 if (!rootElement) throw new Error('Root element not found');
 
-createRoot(rootElement).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+createRoot(rootElement).render(<App />);
